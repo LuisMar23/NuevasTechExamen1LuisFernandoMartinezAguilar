f83c76464f87888a921b0386fba137193d2cce3b
unknown <lusho18fernando@gmail.com>
2024-03-20 19:53:10 -0400

    Modelos e Interfaces

---
+++ Examen1LuisMartinez/Modelos/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1LuisMartinez.Modelos
+{
+    internal class Producto
+    {
+    }
+}
